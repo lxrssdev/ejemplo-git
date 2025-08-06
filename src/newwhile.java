66fc6f1e0fc8725033995081fa1e5fb0de75b4c7
JuanJo <juanjosegomcam@gmail.com>
2025-08-06 00:40:41 -0600

    este es el fibbonacci

--- src/newwhile.java
+++ src/newwhile.java
@@ -2,10 +2,15 @@ public class newwhile {
     public static void main(String[] args) {
         int i=0;
         int j=20;
+        int a=0, b=1;
         while (i<j) {
             i++;
             System.out.println(i);
-            
+            a=b;
+            b=b+a;
+            System.out.println(a);
         }
+
+
     }
 }
