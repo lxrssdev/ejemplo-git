a1723acf28efaaba23948442252592da8d234fd6
JuanJo <juanjosegomcam@gmail.com>
2025-08-06 00:36:18 -0600

    hola este es el while

---
+++ src/newwhile.java
@@ -0,0 +1,11 @@
+public class newwhile {
+    public static void main(String[] args) {
+        int i=0;
+        int j=20;
+        while (i<j) {
+            i++;
+            System.out.println(i);
+            
+        }
+    }
+}
