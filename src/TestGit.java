630071d66f89cf13b7b402262cbfca2f35511232
Jaydeeeveloper <manelitos123@gmail.com>
2025-08-05 23:05:03 -0600

    Contador agregado

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hola git");
+
+        //Primer cambio contador
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
